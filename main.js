a13e0d3307e8a0196f52aebb9d074cb6353e9b8f
bonkbank45 <bankforever22@gmail.com>
2023-08-06 02:26:47 +0700

    Implemented added function

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function add(a,b) {
     return a + b;
 }
@@ -32,8 +33,89 @@ function operate(a,b,operate) {
             console.warn("Invalid case!");
     }
 }
+*/
 
-console.log(add(5,5));
-console.log(subtract(5,5));
-console.log(multiply(5,5));
-console.log(divide(5,5));
+class Calculator {
+    constructor() {}
+    add(a, b) {
+        return a + b;
+    }
+    substract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+    operate(a, b, operate) {
+        switch(operate) {
+            case "+":
+                return this.add(a,b);
+            case "-":
+                return this.substract(a,b);
+            case "*":
+                return this.multiply(a,b);
+            case "/":
+                return this.divide(a,b);
+            default:
+                console.warn("Invalid case!");
+        }
+    }
+    clear() {
+        display.textContent = "";
+        operand1 = "";
+        operand2 = "";
+    }
+    updateDisplay(text) {
+        display.textContent += text;
+    }
+}
+
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+let currentValue = "";
+
+function numberEvent() {
+    currentValue += this.value;
+    calculator.updateDisplay(this.value);
+}
+
+function isAlreadyOperator(value) {
+    if (value.match(/[+\-*\/]/g))
+        return true;
+    return false;
+}
+
+function addedEvent() {
+    if (!isAlreadyOperator(display.textContent.toString())) {
+        operand1 = currentValue;
+        currentValue = "";
+        operator = "+";
+        calculator.updateDisplay(operator);
+    }
+    return;
+}
+
+function equalEvent() {
+    if (operator === "") return;
+    operand2 = currentValue;
+    currentValue = calculator.operate(Number(operand1), Number(operand2), operator);
+    calculator.clear();
+    calculator.updateDisplay(currentValue);
+}
+
+const calculator = new Calculator();
+const buttonsNumber = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+const added = document.querySelector("#add");
+const equal = document.querySelector("#equal");
+
+buttonsNumber.forEach((button) => {
+    button.addEventListener("click", numberEvent);
+});
+
+added.addEventListener("click", addedEvent);
+equal.addEventListener("click", equalEvent);
