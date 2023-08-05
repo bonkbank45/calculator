eff3a11c2f305ffd07dc2d64ede0beac95385ed4
bonkbank45 <bankforever22@gmail.com>
2023-08-05 16:27:09 +0700

    Create layout calculator

--- main.js
+++ main.js
@@ -14,6 +14,25 @@ function divide(a,b) {
     return a / b;
 }
 
+function operate(a,b,operate) {
+    switch (operate) {
+        case 1:
+            add(a,b);
+            break;
+        case 2:
+            subtract(a,b);
+            break;
+        case 3:
+            multiply(a,b);
+            break;
+        case 4:
+            divide(a,b);
+            break;
+        default:
+            console.warn("Invalid case!");
+    }
+}
+
 console.log(add(5,5));
 console.log(subtract(5,5));
 console.log(multiply(5,5));
