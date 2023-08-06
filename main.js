ca95737177e40b509022174ec4c9dca4fa97e09a
bonkbank45 <bankforever22@gmail.com>
2023-08-06 14:31:07 +0700

    Change based code to OOP

--- main.js
+++ main.js
@@ -1,42 +1,10 @@
-/*
-function add(a,b) {
-    return a + b;
-}
-
-function subtract(a,b) {
-    return a - b;
-}
-
-function multiply(a,b) {
-    return a * b;
-}
-
-function divide(a,b) {
-    return a / b;
-}
-
-function operate(a,b,operate) {
-    switch (operate) {
-        case 1:
-            add(a,b);
-            break;
-        case 2:
-            subtract(a,b);
-            break;
-        case 3:
-            multiply(a,b);
-            break;
-        case 4:
-            divide(a,b);
-            break;
-        default:
-            console.warn("Invalid case!");
-    }
-}
-*/
-
 class Calculator {
-    constructor() {}
+    constructor() {
+        this.operand1 = "";
+        this.operand2 = "";
+        this.operator = "";
+        this.currentValue = "";
+    }
     add(a, b) {
         return a + b;
     }
@@ -63,48 +31,40 @@ class Calculator {
                 console.warn("Invalid case!");
         }
     }
+    numberEvent(button) {
+        this.currentValue += button;
+        this.updateDisplay(button);
+    }
+    addedEvent(button){
+        if (!isAlreadyOperator(display.textContent.toString())) {
+            this.operand1 = this.currentValue;
+            this.currentValue = "";
+            this.operator = button;
+            calculator.updateDisplay(this.operator);
+        }
+        return;
+    }
+    equalEvent(button) {
+        if (this.operator === "") return;
+        this.operand2 = this.currentValue;
+        this.currentValue = calculator.operate(Number(this.operand1), Number(this.operand2), this.operator);
+        calculator.clear();
+        calculator.updateDisplay(this.currentValue);
+    }
     clear() {
         display.textContent = "";
-        operand1 = "";
-        operand2 = "";
+        this.operand1 = "";
+        this.operand2 = "";
     }
     updateDisplay(text) {
         display.textContent += text;
     }
 }
 
-let operand1 = "";
-let operand2 = "";
-let operator = "";
-let currentValue = "";
-
-function numberEvent() {
-    currentValue += this.value;
-    calculator.updateDisplay(this.value);
-}
-
 function isAlreadyOperator(value) {
     if (value.match(/[+\-*\/]/g))
         return true;
     return false;
-}
-
-function addedEvent() {
-    if (!isAlreadyOperator(display.textContent.toString())) {
-        operand1 = currentValue;
-        currentValue = "";
-        operator = "+";
-        calculator.updateDisplay(operator);
-    }
-    return;
-}
-
-function equalEvent() {
-    if (operator === "") return;
-    operand2 = currentValue;
-    currentValue = calculator.operate(Number(operand1), Number(operand2), operator);
-    calculator.clear();
-    calculator.updateDisplay(currentValue);
 }
 
 const calculator = new Calculator();
@@ -114,8 +74,8 @@ const added = document.querySelector("#add");
 const equal = document.querySelector("#equal");
 
 buttonsNumber.forEach((button) => {
-    button.addEventListener("click", numberEvent);
+    button.addEventListener("click", (button) => { calculator.numberEvent(button.target.value); });
 });
 
-added.addEventListener("click", addedEvent);
-equal.addEventListener("click", equalEvent);
+added.addEventListener("click", (button) => { calculator.addedEvent(button.target.value); });
+equal.addEventListener("click", (button) => { calculator.equalEvent(button.target.value); });
