bba9a0ff719840ad4d3b76e1457439fb7755fa44
bonkbank45 <bankforever22@gmail.com>
2023-08-06 17:31:52 +0700

    Implement substract function

--- main.js
+++ main.js
@@ -40,39 +40,59 @@ class Calculator {
             this.completeCal = false; 
         };
         this.currentValue += button;
-        this.updateDisplay(button);
+        this.updateDisplay(button, false);
     }
     addedEvent(button){
         if (!isAlreadyOperator(display.textContent.toString())) {
             this.operand1 = this.currentValue;
             this.currentValue = "";
             this.operator = button;
-            calculator.updateDisplay(this.operator);
+            calculator.updateDisplay(this.operator, false);
+        } else {
+            this.operator = button;
+            calculator.updateDisplay(changeAlreadyOperator(display.textContent.toString(),this.operator), true);
         }
-        return;
+    }
+    subbedEvent(button){
+        if (!isAlreadyOperator(display.textContent.toString())) {
+            this.operand1 = this.currentValue;
+            this.currentValue = "";
+            this.operator = button;
+            calculator.updateDisplay(this.operator, false);
+        } else {
+            this.operator = button;
+            calculator.updateDisplay(changeAlreadyOperator(display.textContent.toString(),this.operator), true);
+        }
     }
     equalEvent(button) {
-        if (this.operator === "") return;
+        if (this.operator === "" || this.currentValue === "" || this.operand1 === "") return;
         this.operand2 = this.currentValue;
         this.currentValue = calculator.operate(Number(this.operand1), Number(this.operand2), this.operator);
         this.completeCal = true;
         calculator.clear();
-        calculator.updateDisplay(this.currentValue);
+        calculator.updateDisplay(this.currentValue, false);
     }
     clear() {
         display.textContent = "";
         this.operand1 = "";
         this.operand2 = "";
     }
-    updateDisplay(text) {
-        display.textContent += text;
+    updateDisplay(text, hardUpdate) {
+        if (hardUpdate === false)
+            display.textContent += text;
+        if (hardUpdate === true)
+            display.textContent = text;
     }
 }
 
 function isAlreadyOperator(value) {
-    if (value.match(/[+\-*\/]/g))
+    if (value.match(/\d+[+\-*\/]/g))
         return true;
     return false;
+}
+
+function changeAlreadyOperator(text, operator) {
+    return text.replace(/[+\-*\/]/g, operator);
 }
 
 const calculator = new Calculator();
@@ -80,6 +100,7 @@ const buttonsNumber = document.querySelectorAll(".number");
 const display = document.querySelector(".display");
 const added = document.querySelector("#add");
 const equal = document.querySelector("#equal");
+const subbed = document.querySelector("#sub");
 
 buttonsNumber.forEach((button) => {
     button.addEventListener("click", (button) => { calculator.numberEvent(button.target.value); });
@@ -87,3 +108,4 @@ buttonsNumber.forEach((button) => {
 
 added.addEventListener("click", (button) => { calculator.addedEvent(button.target.value); });
 equal.addEventListener("click", (button) => { calculator.equalEvent(button.target.value); });
+subbed.addEventListener("click", (button) => { calculator.subbedEvent(button.target.value); });
