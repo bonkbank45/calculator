0f594ff8935ac9f8318098011d7c16782666c7a3
bonkBANK <bankforever22@gmail.com>
2023-08-11 00:39:23 +0700

    Edit method name&encapsulation

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 class Calculator {
-    constructor() {
-        this.operand1 = "";
-        this.operand2 = "";
-        this.operator = "";
-        this.currentValue = "";
-        this.isCompleteCal = false;
-        this.isError = false;
-    }
-    add(a, b) {
-        return a + b;
-    }
-    substract(a, b) {
-        return a - b;
-    }
-    multiply(a, b) {
-        return a * b;
-    }
-    divide(a, b) {
-        return a / b;
-    }
-    operate(a, b, operate) {
+    #operand1 = "";
+    #operand2 = "";
+    #operator = "";
+    #currentValue = "";
+    #isCompleteCal = false;
+    #isError = false;
+
+    add = (a, b) => a + b;
+    substract = (a, b) => a - b;
+    multiply = (a, b) => a * b;
+    divide = (a, b) => a / b;
+
+    #operate(a, b, operate) {
         switch(operate) {
             case "+":
                 return this.add(a,b);
@@ -33,122 +25,122 @@ class Calculator {
                 console.warn("Invalid case!");
         }
     }
-    addNum(button) {
+    onNumberButtonClick(button) {
         // if after calculate, u are clicking number (not operation!), it will clear text number display.
-        if (this.isCompleteCal === true && !this.isAlreadyOperator(display.textContent.toString())) { 
-            this.clearOperand();
-            this.clearDisplay();
-            this.currentValue = "";
-            this.isCompleteCal = false;
-            this.isError = false;
+        if (this.#isCompleteCal === true && !this.#isAlreadyOperator(display.textContent.toString())) { 
+            this.#clearOperand();
+            this.#clearDisplay();
+            this.#currentValue = "";
+            this.#isCompleteCal = false;
+            this.#isError = false;
         } 
-        if (this.isError === false) {
-            this.currentValue += button;
-            this.addCharToDisplay(button, false);
+        if (this.#isError === false) {
+            this.#currentValue += button;
+            this.#addCharToDisplay(button, false);
         }
     }
-    addDecimalPoint(button){
+    onDecimalPointButtonClick(button){
         // Check - it doesn't have another decimalpoint
         if (this.currentValue.match(/\./g)) {
             return;
         }
-        if (this.currentValue === "") {
-            this.currentValue = "0" + button;
-            this.addCharToDisplay(this.currentValue, false);
+        if (this.#currentValue === "") {
+            this.#currentValue = "0" + button;
+            this.#addCharToDisplay(this.#currentValue, false);
             return;
         }
-        if (this.isCompleteCal === true) {
-            this.addNum("0" + button);
+        if (this.#isCompleteCal === true) {
+            this.onNumberButtonClick("0" + button);
             return;
         }
-        if (this.isError === false) {
-            this.addNum(button);
+        if (this.#isError === false) {
+            this.onNumberButtonClick(button);
         }
     }
-    equalEvent() {
-        if (this.operator === "" || this.currentValue === "" || this.operand1 === "") return 0;
-        this.operand2 = this.currentValue;
-        this.currentValue = this.operate(Number(this.operand1), Number(this.operand2), this.operator).toString();
-        this.isCompleteCal = true;
-        this.clearDisplay();
-        this.clearOperand();
-        this.clearOperator();
-        if (this.errorStateCheck(this.currentValue)){
-            this.isCompleteCal = true;
+    onEqualButtonClick() {
+        if (this.#operator === "" || this.#currentValue === "" || this.#operand1 === "") return 0;
+        this.#operand2 = this.#currentValue;
+        this.#currentValue = this.#operate(Number(this.#operand1), Number(this.#operand2), this.#operator).toString();
+        this.#isCompleteCal = true;
+        this.#clearDisplay();
+        this.#clearOperand();
+        this.#clearOperator();
+        if (this.#errorStateCheck(this.#currentValue)){
+            this.#isCompleteCal = true;
             return 0;
         }
-        this.addCharToDisplay(this.currentValue, false);
+        this.#addCharToDisplay(this.#currentValue, false);
     }
-    clearOperand() {
-        this.operand1 = "";
-        this.operand2 = "";
+    onAllClearButtonClick() {
+        this.#currentValue = "";
+        this.#isCompleteCal = false;
+        this.#isError = false;
+        this.#clearOperand()
+        this.#clearDisplay();
     }
-    clearOperator() {
-        this.operator = "";
-    }
-    allClear() {
-        this.isCompleteCal = false;
-        this.isError = false;
-        this.clearOperand()
-        this.clearDisplay();
-        this.currentValue = "";
-    }
-    removeLastCharacter() {
-        //123 || single number
+    onDeleteButtonClick() {
+        // pattern 123 || single number
         if (/^\d+[\d]$/g.test(display.textContent) || /(^|\D)\d(\D|$)]/.test(display.textContent)) {
-            this.currentValue = this.currentValue.replace(/.$/g, "");
+            this.#currentValue = this.#currentValue.replace(/.$/g, "");
         }
-        //regex update currentValue for ( . ) 
+        // pattern for update currentValue for (12.3) 
         if (/^\d+.\d+$/g.test(display.textContent)) {
-            this.currentValue = this.currentValue.replace(/.$/g, "");
+            this.#currentValue = this.#currentValue.replace(/.$/g, "");
         }
-        //123+ || -123-
+        // pattern 123+ || -123-
         if (/^(\d|-)+[+\-*\/.]$/g.test(display.textContent)) {
-            this.operator = this.operator.replace(/.$/g, "");
-            this.operand1 = "";
-            this.currentValue = display.textContent.match(/[0-9.]+/g).toString().replaceAll(",", "");
+            this.#operator = this.#operator.replace(/.$/g, "");
+            this.#operand1 = "";
+            this.#currentValue = display.textContent.match(/[0-9.]+/g).toString().replaceAll(",", "");
         }
-        //0.+-*/
+        // pattern 0.+-*/
         if (/0.[+\-*\/.]$/g.test(display.textContent)) {
-            this.operand1 = "";
-            this.currentValue = display.textContent.match(/0/g).toString().replaceAll(",", "");
+            this.#operand1 = "";
+            this.#currentValue = display.textContent.match(/0/g).toString().replaceAll(",", "");
         }
-        //123+123
+        //pattern 123+123
         if (/\d+[+\-*\/]\d+/g.test(display.textContent)) {
-            this.currentValue = this.currentValue.replace(/.$/g, "");
+            this.#currentValue = this.#currentValue.replace(/.$/g, "");
         }
-        //123+0.
+        //pattern 123+0.
         if (/^0.[+\-*\/]0./g.test(display.textContent)) {
-            this.currentValue = this.currentValue.replace(/.$/g, "");
+            this.#currentValue = this.#currentValue.replace(/.$/g, "");
         }
-        if (this.isError === false) {
+        if (this.#isError === false) {
             display.textContent = display.textContent.replace(/.$/g, "");
         }
     }
-    updateOperator(operator) {
-        if (this.isError === false) {
-            if (!this.isAlreadyOperator(display.textContent) && display.textContent !== "" ) {
-                this.operand1 = this.currentValue;
-                this.currentValue = "";
-                this.operator = operator;
-                this.addCharToDisplay(this.operator, false);
+    onOperatorButtonClick(operator) {
+        if (this.#isError === false) {
+            if (!this.#isAlreadyOperator(display.textContent) && display.textContent !== "" ) {
+                this.#operand1 = this.#currentValue;
+                this.#currentValue = "";
+                this.#operator = operator;
+                this.#addCharToDisplay(this.#operator, false);
             }
             // 12+ and click operator
-            if (this.isAlreadyOperator(display.textContent) && this.currentValue === "" && this.operand1 !== ""){
-                this.operator = operator;
-                this.addCharToDisplay(this.changeAlreadyOperator(display.textContent.toString(),this.operator), true);
+            if (this.#isAlreadyOperator(display.textContent) && this.#currentValue === "" && this.#operand1 !== ""){
+                this.#operator = operator;
+                this.#addCharToDisplay(this.#changeAlreadyOperator(display.textContent.toString(),this.#operator), true);
             }
             // 12+3 and click operator
-            if (this.isAlreadyOperator(display.textContent) && this.currentValue !== "" && this.operand1 !== ""){
-                this.equalEvent();
-                this.operand1 = this.currentValue;
-                this.currentValue = "";
-                this.operator = operator;
-                this.addCharToDisplay(this.operator,false);
+            if (this.#isAlreadyOperator(display.textContent) && this.#currentValue !== "" && this.#operand1 !== ""){
+                this.onEqualButtonClick();
+                this.#operand1 = this.#currentValue;
+                this.#currentValue = "";
+                this.#operator = operator;
+                this.#addCharToDisplay(this.#operator,false);
             }
         }
     }
-    addCharToDisplay(text, hardUpdate) {
+    #clearOperand() {
+        this.#operand1 = "";
+        this.#operand2 = "";
+    }
+    #clearOperator() {
+        this.#operator = "";
+    }
+    #addCharToDisplay(text, hardUpdate) {
         if (hardUpdate === false) {
             if(display.textContent === "0" && /[+\-*\/]/.test(text)) {
                 display.textContent += text;
@@ -167,26 +159,26 @@ class Calculator {
         if (hardUpdate === true)
             display.textContent = text;
     }
-    clearDisplay() {
+    #clearDisplay() {
         display.textContent = "";
     }
-    isAlreadyOperator(value) {
+    #isAlreadyOperator(value) {
         if (value.match(/[\d.]+[+\-*\/]/g))
             return true;
         return false;
     }
-    changeAlreadyOperator(text, operator) {
+    #changeAlreadyOperator(text, operator) {
         return text.replace(/[+\-*\/]$/g, operator);
     }
-    errorStateCheck(error) {
+    #errorStateCheck(error) {
         if (error == "NaN") {
             display.textContent = "Result is undefined";
-            this.isError = true;
+            this.#isError = true;
             return true;
         }
         if (error == "Infinity" || error == "-Infinity") {
             display.textContent = "Cannot divide by zero";
-            this.isError = true;
+            this.#isError = true;
             return true;
         }
         return false;
@@ -206,14 +198,14 @@ const backspace = document.getElementById("backspace");
 const dot = document.getElementById("dot");
 
 buttonsNumber.forEach((button) => {
-    button.addEventListener("click", (button) => { calculator.addNum(button.target.value); });
+    button.addEventListener("click", (button) => { calculator.onNumberButtonClick(button.target.value); });
 });
 
-added.addEventListener("click", (button) => { calculator.updateOperator(button.target.value); });
-equal.addEventListener("click", () => { calculator.equalEvent(); });
-subbed.addEventListener("click", (button) => { calculator.updateOperator(button.target.value); });
-mutiply.addEventListener("click", (button) => { calculator.updateOperator(button.target.value); });
-divide.addEventListener("click", (button) => { calculator.updateOperator(button.target.value); });
-clear.addEventListener("click", () => { calculator.allClear(); });
-backspace.addEventListener("click", () => { calculator.removeLastCharacter(); } );
-dot.addEventListener("click", (button) => { calculator.addDecimalPoint(button.target.value); });
+added.addEventListener("click", (button) => { calculator.onOperatorButtonClick(button.target.value); });
+equal.addEventListener("click", () => { calculator.onEqualButtonClick(); });
+subbed.addEventListener("click", (button) => { calculator.onOperatorButtonClick(button.target.value); });
+mutiply.addEventListener("click", (button) => { calculator.onOperatorButtonClick(button.target.value); });
+divide.addEventListener("click", (button) => { calculator.onOperatorButtonClick(button.target.value); });
+clear.addEventListener("click", () => { calculator.onAllClearButtonClick(); });
+backspace.addEventListener("click", () => { calculator.onDeleteButtonClick(); } );
+dot.addEventListener("click", (button) => { calculator.onDecimalPointButtonClick(button.target.value); });
